8debd194276a12152e116c2a5485eec432967e0d
zeeshan <zeeshanachra1@gmail.com>
2024-03-20 02:29:58 +0500

    use modules and import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,8 @@
-
+import { cart } from "../data/cart.js";
 let productsHTML = '';
 products.forEach((product) => {
-    productsHTML +=
-        `
+  productsHTML +=
+    `
         <div class="product-container">
         <div class="product-image-container">
           <img class="product-image"
@@ -54,43 +54,43 @@ products.forEach((product) => {
 
 
 `
-    // console.log(productsHTML)
+  // console.log(productsHTML)
 })
 // console.log(productsHTML)
 document.querySelector('.products-grid').innerHTML = productsHTML
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-    button.addEventListener('click', () => {
-        const productId = button.dataset.productId;
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
 
-        let matchingItem;
-        cart.forEach((item) => {
-            if (productId === item.productId) {
-                matchingItem = item
-            }
-        })
-
-        if (matchingItem) {
-            matchingItem.quantity += 1
-        }
-        else {
-            cart.push({
-                productId: productId,
-                quantity: 1
-            })
-            let cartQuantity = 0
-            cart.forEach((item) => {
-                cartQuantity += item.quantity
-
-            })
-            let results = document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
-            console.log(results)
-
-
-            // console.log(cart)
-
-        }
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item
+      }
     })
+
+    if (matchingItem) {
+      matchingItem.quantity += 1
+    }
+    else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      })
+      let cartQuantity = 0
+      cart.forEach((item) => {
+        cartQuantity += item.quantity
+
+      })
+      let results = document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+      console.log(results)
+
+
+      // console.log(cart)
+
+    }
+  })
 
 })
 
